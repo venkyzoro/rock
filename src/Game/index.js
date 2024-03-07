df49e07ebd547415426758708cc1a90c02458fd3
venkyzoro <venkykiran76@gamail.com>
2024-03-07 14:46:42 +0000

    solution

---
+++ src/Game/index.js
@@ -0,0 +1,171 @@
+// eslint-disable-next-line
+import {Component} from 'react'
+import Popup from 'reactjs-popup'
+import {RiCloseLine} from 'react-icons/ri'
+import Buttons from '../Buttons'
+import GameResultsView from '../GameResultsView'
+import 'reactjs-popup/dist/index.css'
+import {
+  MainContainer,
+  ScoreContainer,
+  ItemsContainer,
+  Heading,
+  ScoreCardContainer,
+  ParagraphScore,
+  ScoreSpan,
+  ItemsImagesContainer,
+  // eslint-disable-next-line no-unused-vars
+  PlayAgainButton,
+  PopUpContainer,
+  PopUpButton,
+  RulesImageContainer,
+  RulesImage,
+  ClosedLineContainer,
+  ClosedLineButton,
+} from './styledComponents'
+
+class Game extends Component {
+  state = {
+    showResult: false,
+    myChoice: {},
+    apponentChoice: {},
+    score: 0,
+    resultMessage: '',
+  }
+
+  onClickPlayAgain = () => this.setState({showResult: false})
+
+  onGetResult = () => {
+    const {myChoice, apponentChoice, resultMessage} = this.state
+    // eslint-disable-next-line no-unused-vars
+    const {id, image} = apponentChoice
+    return (
+      <GameResultsView
+        myChoice={myChoice}
+        apponentChoice={apponentChoice}
+        resultMessage={resultMessage}
+        playAgain={this.onClickPlayAgain}
+      />
+    )
+  }
+
+  onGetButtonId = (id, image) => {
+    const {choicesList} = this.props
+    const number = Math.floor(Math.random() * choicesList.length)
+    if (choicesList[number].id === 'ROCK' && id === 'SCISSORS') {
+      this.setState(prevState => ({
+        showResult: true,
+        myChoice: [id, image],
+        apponentChoice: choicesList[number],
+        score: prevState.score - 1,
+        resultMessage: 'YOU LOSE',
+      }))
+    } else if (choicesList[number].id === 'ROCK' && id === 'PAPER') {
+      this.setState(prevState => ({
+        showResult: true,
+        myChoice: [id.image],
+        apponentChoice: choicesList[number],
+        score: prevState.score + 1,
+        resultMessage: 'YOU WON',
+      }))
+    } else if (choicesList[number].id === 'SCISSORS' && id === 'ROCK') {
+      this.setState(prevState => ({
+        showResult: true,
+        myChoice: [id.image],
+        apponentChoice: choicesList[number],
+        score: prevState.score + 1,
+        resultMessage: 'YOU WON',
+      }))
+    } else if (choicesList[number].id === 'SCISSORS' && id === 'PAPER') {
+      this.setState(prevState => ({
+        showResult: true,
+        myChoice: [id.image],
+        apponentChoice: choicesList[number],
+        score: prevState.score - 1,
+        resultMessage: 'YOU LOSE',
+      }))
+    } else if (choicesList[number].id === 'PAPER' && id === 'ROCK') {
+      this.setState(prevState => ({
+        showResult: true,
+        myChoice: [id.image],
+        apponentChoice: choicesList[number],
+        score: prevState.score - 1,
+        resultMessage: 'YOU LOSE',
+      }))
+    } else if (choicesList[number].id === 'PAPER' && id === 'SCISSORS') {
+      this.setState(prevState => ({
+        showResult: true,
+        myChoice: [id.image],
+        apponentChoice: choicesList[number],
+        score: prevState.score + 1,
+        resultMessage: 'YOU WON',
+      }))
+    } else {
+      this.setState({
+        showResult: true,
+        myChoice: [id, image],
+        apponentChoice: choicesList[number],
+        resultMessage: 'IT IS DRAW',
+      })
+    }
+  }
+
+  onGetImages = () => {
+    const {choicesList} = this.props
+    return (
+      <ItemsImagesContainer>
+        {choicesList.map(eachItem => (
+          <Buttons
+            key={eachItem.id}
+            buttonDetails={eachItem}
+            onGetId={this.onGetButtonId}
+          />
+        ))}
+      </ItemsImagesContainer>
+    )
+  }
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const {showResult, score, myChoice, apponentChoice} = this.state
+    return (
+      <MainContainer>
+        <ScoreContainer>
+          <ItemsContainer>
+            <Heading>
+              ROCK
+              <br />
+              PAPER
+              <br />
+              SCISSORS
+            </Heading>
+          </ItemsContainer>
+          <ScoreCardContainer>
+            <ParagraphScore>Score</ParagraphScore>
+            <ScoreSpan>{score}</ScoreSpan>
+          </ScoreCardContainer>
+        </ScoreContainer>
+        {showResult ? this.onGetResult() : this.onGetImage()}
+        <PopUpContainer>
+          <Popup modal trigger={<PopUpButton type="button">Rules</PopUpButton>}>
+            {close => (
+              <RulesImageContainer>
+                <ClosedLineContainer>
+                  <ClosedLineButton type="button" onClick={() => close()}>
+                    <RiCloseLine />
+                  </ClosedLineButton>
+                </ClosedLineContainer>
+                <RulesImage
+                  src="https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rules-image.png"
+                  alt="rules"
+                />
+              </RulesImageContainer>
+            )}
+          </Popup>
+        </PopUpContainer>
+      </MainContainer>
+    )
+  }
+}
+
+export default Game
